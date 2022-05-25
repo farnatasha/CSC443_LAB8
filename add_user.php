1e8adc6e7129c90a9314292f91bd1288d5acc026
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 13:29:55 +0800

    6

--- add_user.php
+++ add_user.php
@@ -5,7 +5,7 @@ $sql="insert into user (first_name,last_name,email,password)
 values('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]',
 '$_POST[reg_date]')";
 if(!mysqli_query($conn, $sql)){
-    echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    echo "Failed to connect to MySQL: ";
     exit();
 }
 echo "1 record successfully added...";
