e2b1f8c622ba92dda74b64997895cff3041f1d49
farnatasha <2020898386@student.uitm.edu.my>
2022-05-25 11:22:39 +0800

    1 commit

---
+++ add_user.php
@@ -0,0 +1,12 @@
+<?php
+include 'conn.php';
+
+$sql="insert into user (first_name,last_name,email,password)
+values('$_POST[firstname]','$_POST[lastname]','$_POST[email]','$_POST[password]',
+'$_POST[reg_date]')";
+if(!mysqli_query($conn, $sql)){
+    die('Error: '.mysqli_error());
+}
+echo "1 record successfully added...";
+
+?>
